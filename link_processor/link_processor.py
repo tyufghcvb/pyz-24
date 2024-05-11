fb5ad223cab345b19302a8fd90d0e99d578bbde4
nackt <nackthasslichundeinfachgenial@gmail.com>
2024-05-11 13:50:41 +0200

    recognize_url_type and basde_url fix

--- link_processor/link_processor.py
+++ link_processor/link_processor.py
@@ -1,20 +1,44 @@
+import re
+
+url_base = r'^https://helion\.pl/'
+
+
+def is_valid_url():
+    pass
+
 
 class LinkProcessor:
-    def __init__(self, urls):
-        self.urls = urls
+    def __init__(self, user_id, user_url):
+        self.user_id = user_id
+        self.user_url = user_url
 
+    def generate_dedicated_link(url_type):
+        pass
 
+    # patterns list
 
+    # patterns_list = {
+    #     'url_base': url_base,
+    #     # `https://helion.pl/ksiazki/nazwa_ksiazki`
+    #     'books': f'{url_base}ksiazki/[\w_]+$',
+    #     # `https://helion.pl/kategorie/programowanie'
+    #     'category': f'{url_base}kategorie/[\w]+$',
+    #     # `https://helion.pl/kategorie/promocja-xyz`
+    #     'sales': f'{url_base}kategorie/[\w]+-[\w]+$'
+    # }
 
-def generate_dedicated_link(clientid, url):
-    pass
-#return modified urls
+    def recognize_url_type(self, patterns):
+        print(f"URL: {self.user_url}")
+        for key, value in patterns.items():
+            if re.match(value, self.user_url):
+                url_type = key
+                print(f"URL: {self.user_url} is {key} type")
+                return url_type
 
+        print("poza kategorią")
+        return None
 
-def recognize_url_type(url):
-    pass
     # Strona główna
     # Strona produktu
     # Strona promocji
-    # Dodanie zestawu do koszyka
     # Link do kategorii
--- link_processor/main.py
+++ link_processor/main.py
@@ -1,9 +1,33 @@
+from link_processor import LinkProcessor, url_base
+
+
 def main():
-    # ext: load
-    #    urls = load_urls_from_file('urls.csv')
+
+    patterns_list = {
+        'url_base': f'url_base$',
+        # `https://helion.pl/ksiazki/nazwa_ksiazki`
+        'books': f'{url_base}ksiazki/[\w_]+$',
+        # `https://helion.pl/kategorie/programowanie'
+        'category': f'{url_base}kategorie/[\w]+$',
+        # `https://helion.pl/kategorie/promocja-xyz`
+        'sales': f'{url_base}kategorie/[\w]+-[\w]+$'
+    }
 
     user_id = input("please put your user_id")
-    user_url = input("please put your url")
+    # user_url = input("please put your url")
+    user_url = "https://helion.pl/kategorie/promocja-xyz"
+
+    link = LinkProcessor(user_id, user_url)
+    url_type = link.recognize_url_type(patterns_list)
+
+    print(url_type)
+
+    urls_test = [
+        "https://helion.pl/ksiazki/nazwa_ksiazki",
+        "https://helion.pl/kategorie/promocja-xyz",
+        "https://helion.pl/kategorie/programowanie",
+        "https://helion.pl/ksiazki/inna_ksiazka"
+    ]
 
 
 if __name__ == "__main__":
